0c9303808ceae33b65af9571b1038f18325747ae
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2018-12-29 19:59:01 +0900

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Tsuyoshi Yamaguchi on 2018/12/29.
+//  Copyright © 2018 ronoya442. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
